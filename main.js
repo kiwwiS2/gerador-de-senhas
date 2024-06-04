65dd53e6df5d215b00ffa31f184ede42abda76c6
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-06-04 11:34:09 -0300

    main.js

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-const numeroSenha = Document.querySelector('.parametro-senha__texto');
-let tamanho = 12;
-numeroSenha.textoContent = tamanhpsenha;
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-botoes [0].onclick = diminuiTamanho;
-botoes [1].onclick = aumentaTamanh0;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho{
+function diminuiTamanho(){
     if (tamanhoSenha < 1){
         tamanhoSenha--;
     }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
